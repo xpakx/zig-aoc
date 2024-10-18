c5e7a1667588938e5dc394cbb3f1f9f619846471
xpakx <55586470+xpakx@users.noreply.github.com>
2024-10-18 15:24:43 +0200

    feat: solve 2nd star of day 2

--- src/day02.zig
+++ src/day02.zig
@@ -26,7 +26,7 @@ pub fn main() !void {
     defer arr.deinit();
 
     var total_area: u32 = 0;
-
+    var total_ribbon: u32 = 0;
 
     while (true) {
         reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch (err) {
@@ -56,16 +56,25 @@ pub fn main() !void {
         const side3 = box.width * box.height;
         const area = 2 * side1 + 2 * side2 + 2 * side3;
         const shortest_side = min(side1, min(side2, side3));
-
         total_area += area + shortest_side;
+
+        const longest_dim = max(box.length, max(box.width, box.height));
+        const ribbon_len = 2 * box.length + 2 * box.width + 2 * box.height - 2 * longest_dim;
+        const bow = box.length * box.height * box.width;
+        total_ribbon += ribbon_len + bow;
 
         arr.clearRetainingCapacity();
     }
 
     try stdout.print("Area {d}\n", .{total_area});
+    try stdout.print("Ribbon {d}\n", .{total_ribbon});
     try bw.flush();
 }
 
 fn min(a: u32, b: u32) u32 {
     return if (a < b) a else b;
+}
+
+fn max(a: u32, b: u32) u32 {
+    return if (a > b) a else b;
 }
