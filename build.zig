cbb335293566fd169a5612a6b7ec91202d2a501a
xpakx <55586470+xpakx@users.noreply.github.com>
2024-10-18 15:18:36 +0200

    chore: create build system for aoc
    e.g. `zig build day01` for day one

---
+++ build.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    var day: u32 = 1;
+    while (day <= 3) : (day += 1) {
+        const dayString = b.fmt("day{:0>2}", .{day});
+        const zigFile = b.fmt("src/{s}.zig", .{dayString});
+        const name = b.fmt("{s}", .{dayString});
+
+        const exe = b.addExecutable(.{
+            .name = name,
+            .root_source_file = b.path(zigFile),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        b.installArtifact(exe);
+
+        const run_cmd = b.addRunArtifact(exe);
+
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step(dayString, "Run the app");
+        run_step.dependOn(&run_cmd.step);
+    }
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
